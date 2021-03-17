59470743447c1251c2dc2913912b036082cdc6f5
FFFFF12332 <80814959+FFFFF12332@users.noreply.github.com>
2021-03-17 10:14:28 +0200

    Add files via upload

---
+++ res/x5settings.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+|-------------------------------
+|	GENERAL SETTINGS
+|-------------------------------
+*/
+
+$imSettings['general'] = array(
+	'url' => 'http://uu.com',
+	'public_folder' => '',
+	'salt' => '40lj17553l7g2g57587xftr8vw1usuw0xj98glk157a4c932'
+);
+
+// End of file x5settings.php
